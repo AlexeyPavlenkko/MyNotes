f66afaec32fed434e5bf7e82b1b9f79961842c41
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-01 16:50:10 +0300

    Initial Commit

---
+++ MyNotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNotesApp
+//
+//  Created by Алексей Павленко on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
