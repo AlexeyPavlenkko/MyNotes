27dae657ea1b49c38aceb5c0f544f4fd77ef0eac
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-02 12:51:54 +0300

    codeine + two computed properties added

---
+++ MyNotesApp/Models/Note+CoreDataClass.swift
@@ -0,0 +1,23 @@
+//
+//  Note+CoreDataClass.swift
+//  MyNotesApp
+//
+//  Created by Алексей Павленко on 01.09.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+    var title: String {
+        return text.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: .newlines).first ?? "" // returns the first line of the text
+    }
+    
+    var desc: String {
+        var lines = text.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: .newlines)
+        lines.removeFirst()
+        return "\(lastUpdated.format()) \(lines.first ?? "")" // return second line
+    }
+}
